9a00e841b2acc96096eff687ae111fda5bf81f93
Otávio Olímpio <otavioolimpio@outlook.com>
2026-05-15 12:16:35 -0300

    chore: reorganiza estrutura e adiciona backend spring

---
+++ backend/src/main/java/br/ufrpe/ewaster/EwasterApplication.java
@@ -0,0 +1,13 @@
+package br.ufrpe.ewaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwasterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/ufrpe/ewaster/EwasterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrpe.ewaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
